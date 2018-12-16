c619f2975cc8b903bbf7e4696fe0a1195a5b2016
Glauco Pater <>
2018-12-16 17:57:49 +0100

    Fixed logic for active player. Player simplified to Stateless Component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,33 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Player.scss"; 
-export default class Card extends Component {
-    constructor() {
-        super();
-        this.state = {
-            score: 0,
-            name: ""
-        };
+
+const Player = (props) => { 
+    const isActive = () => {
+        return parseInt(props.name) === props.activePlayer;
     }
-
-
-    componentDidUpdate(prevProps) {
-        if (this.props.reset !== prevProps.reset && this.props.reset) { 
-          this.setState({score: this.state.score + 1})
-        }
-      }
-
-    isActive(){
-        return parseInt(this.props.name) === this.props.activePlayer;
-    }
-
-    render() { 
-        const cssClass = this.isActive() ? "player active" : "player"; 
-
-        console.log(this.props);
+        const cssClass = isActive() ? "player active" : "player";  
         return (
             <div className={cssClass}> 
-                <label>Player {this.props.name}:</label><span> {this.state.score}</span>
+                <label>Player {props.name}:</label><span> {props.score}</span>
             </div>
         );
-    }
 }
+
+export default Player;
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://api.eyeem.com/v2/photos/popular";
 export const X_CLIENT_ID = "9iNUTAc4FCsRj5Co6vJgzVySHxuJtL3Y";
-export const MAX_ITEMS = 4;
-export const DELAY = 1000;
+export const MAX_ITEMS = 8;
+export const ANIMATION_DELAY = 1000;
